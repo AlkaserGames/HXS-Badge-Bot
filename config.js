c336e19a31811bb4811918901e43ac3ee2d13b64
AlkaserGames <51220839+AlkaserGames@users.noreply.github.com>
2023-12-06 22:37:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   clientId: 'hk3y7du9ciwbazgl93dfsyfvmj2lpy', // Replace with your Twitch client ID
   clientSecret: '9pl18ra4zzq74wiz3tjfqbuso7ckcb', // Replace with your Twitch client secret
   roleId: '981367277490409532', // Replace with the actual role ID you want to mention
-  webhookUrl: 'https://discord.com/api/webhooks/1139204955715141752/d-bIDcCKYMtOUqQV0bSBArNoHOdytVAmLPSvhqLxKmn15_ubFwMDKhoDxy6R-uPuO4PO' // Replace with your Discord webhook URL
+  webhookUrl: 'https://discord.com/api/webhooks/1182042951359156305/1bOq5uDovpv09EC9JZHOG43iDlD1CtFTb8nI-03Xqj5RCdYxU5FbD5T-aKslVmwVVsku' // Replace with your Discord webhook URL
 };
